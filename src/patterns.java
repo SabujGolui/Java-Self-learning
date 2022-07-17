a482c499d0f182346117e64127c39c95fc421ae3
Sabuj Golui <sabujgolui5#gmail.com>
2022-07-17 11:14:37 +0530

    added several patterns

--- src/patterns.java
+++ src/patterns.java
@@ -33,16 +33,86 @@ public class patterns {
 //            System.out.println();
 //        }
 
-        //print half pyramid
-        //column value isn't needed
+//        //print half pyramid
+//        //column value isn't needed
+//        for (int i=1;i<=n;i++){
+//            //inner loop
+//            for (int j=1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//
+//        }
+
+//        // INVERTED half pyramid
+//
+//        for (int i=n;i>=1;i--){
+//            //inner loop
+//            for (int j=1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//
+//        }
+
+        // half pyramid (180 degree)
+
+//        for (int i=1;i<=n;i++){
+//                //inner loop for space print
+//            for (int j=1;j<=n-i;j++){
+//                System.out.print(" ");
+//            }
+//            //inner loop for star print
+//            for (int j=1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        // half pyramid with number
+
+//        for (int i=1;i<=n;i++){
+//            for (int j=1;j<=i;j++){
+//                System.out.print(j +" ");
+//            }
+//            System.out.println();
+//        }
+//
+//        // inverted half pyramid with number
+//
+//        for (int i=1;i<=n;i++){
+//            for (int j=1;j<=n-i+1;j++){
+//                System.out.print(j+" ");
+//            }
+//            System.out.println();
+//        }
+
+//        // floyd's triangle
+//
+//        int number =1;
+//        for (int i=1;i<=n;i++){
+//            for (int j=1;j<=i;j++){
+//                 System.out.print(number+" ");
+//                 number++;
+//            }
+//            System.out.println();
+//        }
+
+        // 0-1 triangle
+
         for (int i=1;i<=n;i++){
-            //inner loop
             for (int j=1;j<=i;j++){
-                System.out.print("*");
+                int sum=i+j;
+                if (sum%2 ==0){ //even
+                    System.out.print("1");
+                }else { //odd
+                    System.out.print("0");
+                }
             }
             System.out.println();
-
         }
+
+
 
     }
 
