6db3b9a6e99da76a1bfbe822bd4f6f17ad4afd89
Sabuj Golui <sabujgolui5#gmail.com>
2022-08-05 16:14:42 +0530

    updated

--- src/adpatterns.java
+++ src/adpatterns.java
@@ -7,42 +7,43 @@ public class adpatterns {
         System.out.print("enter the value of rows: ");
         n = sc.nextInt();
 
-//        // butterfly pattern
-//        //upper half
-//        for (int i=1;i<=n;i++){
-//            //first part
-//            for (int j=1; j<=i;j++){
-//                System.out.print("*");
-//            }
-//            // spaces
-//            int space = 2*(n-i);
-//            for (int j=1; j<=space;j++){
-//                System.out.print(" ");
-//            }
-//            // 2nd part
-//            for (int j=1;j<=i;j++){
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//
-//        }
-//
-//        // lower half
-//        for (int i=n;i>=1;i--){
-//            //first part
-//            for (int j=1; j<=i;j++){
-//                System.out.print("*");
-//            }
-//            // spaces
-//            int space = 2*(n-i);
-//            for (int j=1; j<=space;j++){
-//                System.out.print(" ");
-//            }
-//            // 2nd part
-//            for (int j=1;j<=i;j++){
-//                System.out.print("*");
-//            }
-//            System.out.println();
+        // butterfly pattern
+        //upper half
+        for (int i=1;i<=n;i++){
+            //first part
+            for (int j=1; j<=i;j++){
+                System.out.print("*");
+            }
+            // spaces
+            int space = 2*(n-i);
+            for (int j=1; j<=space;j++){
+                System.out.print(" ");
+            }
+            // 2nd part
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+        // lower half
+        for (int i=n;i>=1;i--) {
+            //first part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spaces
+            int space = 2 * (n - i);
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+            // 2nd part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
 //        // solid rhombus
 //        for (int i=1;i<=n;i++){
@@ -114,32 +115,32 @@ public class adpatterns {
 //        }
 
 
-        // diamond pattern (2)
-        // upper half
-        for (int i=1;i<=n;i++){
-            //print spaces
-            for (int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }
-            // print stars
-            for (int j=1;j<=2*i-1;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
-        // lower half
-        for (int i=n-1;i>=1;i--){
-            //print spaces
-            for (int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }
-            // print stars
-            for (int j=1;j<=2*i-1;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+//        // diamond pattern (2)
+//        // upper half
+//        for (int i=1;i<=n;i++){
+//            //print spaces
+//            for (int j=1;j<=n-i;j++){
+//                System.out.print(" ");
+//            }
+//            // print stars
+//            for (int j=1;j<=2*i-1;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//
+//        // lower half
+//        for (int i=n-1;i>=1;i--){
+//            //print spaces
+//            for (int j=1;j<=n-i;j++){
+//                System.out.print(" ");
+//            }
+//            // print stars
+//            for (int j=1;j<=2*i-1;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
 
 
     }
