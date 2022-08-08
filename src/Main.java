8089767f9dd53abed1e6a2c9138d32efada65754
Sabuj Golui <sabujgolui5#gmail.com>
2022-08-08 13:43:51 +0530

    added some code in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int n=0,j=0,sum=0,i;
+        System.out.print("enter the number of values that you want to sum : ");
+        n = sc.nextInt();
+        for (i=0;i<n;i++){
+            System.out.print("enter the value of j :");
+            j=sc.nextInt();
+            sum=sum+j;
+        }
+        System.out.println("the sum is : "+sum);
     }
 }
